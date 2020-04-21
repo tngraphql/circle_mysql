808defd9fa98f6f55e32a84b0d68216b173bf7b2
nguyenpl117 <nguyenpl117@gmail.com>
2020-04-21 09:42:58 +0700

    test knex

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 // MYSQL_HOST=mysql
 // MYSQL_PORT=3306
@@ -7,16 +7,41 @@ var mysql = require('mysql');
 
 // DB_NAME=lucid
 
+function main() {
+    var knex = require('knex')({
+        client: 'mysql',
+        connection: {
+            host: "mysql",
+            user: "virk",
+            password: "password",
+            database: "lucid"
+        }
+    });
 
-var con = mysql.createConnection({
-    host: "mysql",
-    user: "virk",
-    password: "password",
-    database: "lucid"
-});
+    const sql = knex.select('*').from('users')
+        .where(knex.raw('id = ?', [1]))
+        .toSQL();
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+
+    console.log(sql.sql);
+    console.log('select * from `users` where id = ?' === sql.sql);
+    console.log('select * from "users" where id = ?' === sql.sql);
+
+
     process.exit(0);
-});
+}
+
+main();
+
+// var con = mysql.createConnection({
+//     host: "mysql",
+//     user: "virk",
+//     password: "password",
+//     database: "lucid"
+// });
+//
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     process.exit(0);
+// });
